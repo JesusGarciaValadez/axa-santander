1c1456ff0265be67e9a6be3296486929cf6d8fd3
Jesús Antonio García Valadez <zerovamp@hotmail.com>
2013-09-18 13:46:30 -0500

    Class to create an avatar image
    Signed-off-by: Jesús Antonio García Valadez <zerovamp@hotmail.com>

--- Code/snippets/libs/Avatar.php
+++ Code/snippets/libs/Avatar.php
@@ -1,11 +1,7 @@
 <?php
 
-if ( !defined( "IMAGE_PATH" ) ) {
-    
-    define( "IMAGE_PATH", dirname(__FILE__) + DIRECTORY_SEPARATOR + 'avatar' + DIRECTORY_SEPARATOR );
-}
-
-echo 'pipo' + DIRECTORY_SEPARATOR + "huevos";
+if ( !defined( "IMAGE_PATH" ) ) 
+    define( "IMAGE_PATH", dirname(__FILE__) . DIRECTORY_SEPARATOR . 'avatar' . DIRECTORY_SEPARATOR ) ;
 
 class Avatar {
     
@@ -13,18 +9,61 @@ class Avatar {
     private $_shirt;
     private $_pants;
     
-    function __construct ( $putHead = "avatar_cara.png", $putShirt = "avatar_playera.png", $putPants = "avatar_pantalones.png" ) {
+    public function __construct ( $putHead, $putShirt, $putPants ) {
         
-        $this->_head    = IMAGE_PATH + $putHead;
-        $this->_shirt   = IMAGE_PATH + $putShirt;
-        $this->_pants   = IMAGE_PATH + $putPants;
+        $this->_head    = ( !empty( $putHead ) && is_string( $putHead ) ) ? IMAGE_PATH . $putHead : "avatar_cara.png";
+        $this->_shirt   = ( !empty( $putShirt ) && is_string( $putShirt ) ) ? IMAGE_PATH . $putShirt : "avatar_playera.png";
+        $this->_pants   = ( !empty( $putPants ) & is_string( $putPants ) ) ? IMAGE_PATH . $putPants : "avatar_pantalones.png";
     }
     
-    function createAvatar () {
+    /**
+     *
+     *  IMageCreateTrueColor( width, height )
+     *  ImageColorAllocate( image, red, green, blue )
+     *  ImageCreatreFromPng( filename )
+     *  imagecopymerge ( resource $dst_im , resource $src_im , int $dst_x , int $dst_y , int $src_x , int $src_y , int $src_w , int $src_h , int $pct )
+     *
+     */
+    
+    public function createAvatar () {
         
-        $baseImage  = ImageCreateTrueColor( 300, 210 );
+        //Creamos la base de la imagen donde colocaremos luego las otras dos
+        $baseimagen     = imagecreatetruecolor( 105, 217 ) or die( 'Cannot Initilize new GD image stream' );
+        //Le damos un color a la base, en este caso se utiliza el negro
+        $black          = imagecolorallocate( $baseimagen, 250, 250, 250 );
         
+        //Cargamos la segunda imagen(pantalones)
+        $avatarPants    = imagecreatefrompng( $this->_pants );
+        
+        //Juntamos la tercera imagen con la imagen base
+        imagecopymerge( $baseimagen, $avatarPants, 16, 149, 0, 0, 73, 68, 100 );
+        
+        //Cargamos la segunda imagen(cuerpo)
+        $avatarShirt    = imagecreatefrompng( $this->_shirt );
+        
+        //Juntamos la segunda imagen con la imagen base
+        imagecopymerge( $baseimagen, $avatarShirt, 10, 100, 0, 0, 81, 69, 100 );
+        
+        //Cargamos la primera imagen(cabecera)
+        $avatarHead     = imagecreatefrompng( $this->_head );
+        
+        //Unimos la primera imagen con la imagen base
+        imagecopymerge( $baseimagen, $avatarHead, 0, 0, 0, 0, 108, 115, 100 );
+        
+        //Mostramos la imagen en el navegador
+        header( "Content-Type: image/png" );
+        
+        //ImagePng( image, filename, quality, filters )
+        imagepng( $baseimagen );
+        
+        //Limpiamos la memoria utilizada con las imagenes
+        imagedestroy( $logo );
+        imagedestroy( $ts_viewer );
+        imagedestroy( $baseimagen );
     }
 }
+
+$avatar = new Avatar( "avatar_cara.png", "avatar_playera.png", "avatar_pantalones.png");
+$avatar->createAvatar();
 
 ?>
