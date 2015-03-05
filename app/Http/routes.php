4d42b8d2be84d6a3ec5c2468c4e4c8aed6e0df46
Jesus Antonio Garcia Valadez <zerovamp@hotmail.com>
2015-03-05 22:57:18 +0000

    Routes renamed for polls

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,9 +15,9 @@ Route::get( '/', 'RegisterController@index' );
 
 Route::get( '/success', 'UsersController@success' );
 
-Route::get( '/encuesta', 'PollController@show' );
+Route::get( '/encuesta', 'PollController@index' );
 
-Route::get( '/encuesta-enviada', 'PollController@pollSended' );
+Route::get( '/encuesta-enviada', 'PollController@store' );
 
 Route::controllers([
 	'auth' => 'Auth\AuthController',
